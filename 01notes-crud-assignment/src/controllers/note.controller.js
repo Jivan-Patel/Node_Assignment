ff2abf63790877adc8498714c28ad567ce6c116d
Patel Jivan <patel.jivan.cg@gmail.com>
2026-04-17 15:45:15 +0530

    feat: add Get all notes endpoint (GET /api/notes)

--- 01notes-crud-assignment/src/controllers/note.controller.js
+++ 01notes-crud-assignment/src/controllers/note.controller.js
@@ -33,3 +33,19 @@ exports.createNotes = async (req, res) => {
     });
   }
 };
+
+exports.getNotes = async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.status(200).json({
+            success: true,
+            message: "Notes retrieved successfully",
+            data: notes
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
--- 01notes-crud-assignment/src/routes/note.routes.js
+++ 01notes-crud-assignment/src/routes/note.routes.js
@@ -1,9 +1,10 @@
 const Note = require("../models/note.model");
-const { createNote, createNotes } = require("../controllers/note.controller");
+const { createNote, createNotes, getNotes } = require("../controllers/note.controller");
 const express = require("express");
 const router = express.Router();
 
 router.post("/notes", createNotes);
 router.post("/notes/bulk", createNotes);
+router.get("/notes", getNotes);
 
 module.exports = router;
