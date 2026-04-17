40bfd7806d8a7506fc874954d820817bdd9b5fde
Patel Jivan <patel.jivan.cg@gmail.com>
2026-04-17 15:49:35 +0530

    feat: add Get note by id endpoint (GET /api/notes/:id)

--- 01notes-crud-assignment/src/controllers/note.controller.js
+++ 01notes-crud-assignment/src/controllers/note.controller.js
@@ -49,3 +49,20 @@ exports.getNotes = async (req, res) => {
         });
     }
 };
+
+exports.getNoteByID = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const note = await Note.findOne({ _id: id });
+        res.status(200).json({
+            success: true,
+            message: "Note fetched successfully",
+            data: note
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
--- 01notes-crud-assignment/src/routes/note.routes.js
+++ 01notes-crud-assignment/src/routes/note.routes.js
@@ -1,10 +1,11 @@
 const Note = require("../models/note.model");
-const { createNote, createNotes, getNotes } = require("../controllers/note.controller");
+const { createNote, createNotes, getNotes, getNoteByID } = require("../controllers/note.controller");
 const express = require("express");
 const router = express.Router();
 
 router.post("/notes", createNotes);
 router.post("/notes/bulk", createNotes);
 router.get("/notes", getNotes);
+router.get("/notes/:id", getNoteByID);
 
 module.exports = router;
